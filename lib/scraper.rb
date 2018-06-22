c79341bf293732f79ea2fc060f5c13d3f9fbc267
Jed Peek <jedpeek@yahoo.com>
2018-06-22 13:09:06 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = open('./fixtures/student-site/index.html')
     doc = Nokogiri::HTML(html)
-    puts doc.student-name
+    puts doc.css(".student-name")
     #name class .student-name
     #location class .student-location
     #first a tag within .student-card class
