54324fe425c741ec72e01a752374eee62fb542ab
Jed Peek <jedpeek@yahoo.com>
2018-06-22 13:18:07 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,7 @@ class Scraper
     doc = Nokogiri::HTML(html)
     student_names = doc.css(".student-name").text
     student_locations = doc.css(".student-location").text
-     puts student_link = doc.css(".student-card a")
+     puts student_link = doc.css("a .student-card ")
     #name class .student-name
     #location class .student-location
     #first a tag within .student-card class
